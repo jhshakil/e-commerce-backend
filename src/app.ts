43dd9071cbe2da5205b4d06d1d9a8045b4ad7996
jhshakil <shakil11271@gmail.com>
2024-05-21 01:52:52 +0600

    create a product

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application } from 'express';
 import cors from 'cors';
+import { ProductRoutes } from './modules/product/product.route';
 
 const app: Application = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
+
+// application routes
+app.use('/api/products', ProductRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
